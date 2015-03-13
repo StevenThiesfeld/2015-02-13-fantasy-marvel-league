856a4f2d4f2f8f99dd33b0d6b6faf5cdac853f3b
Steven Thiesfeld <scthiesfeld@hotmail.com>
2015-03-13 09:42:33 -0500

    added configure :production in main

--- main.rb
+++ main.rb
@@ -25,5 +25,18 @@ require_relative "controllers/trade"
 configure :development do
   set :database, {adapter: "sqlite3", database: "f_m_l.db"}
 end
+
+configure :production do
+ db = URI.parse(ENV['DATABASE_URL'])
+ ActiveRecord::Base.establish_connection(
+ :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+ :host => db.host,
+ :username => db.user,
+ :password => db.password,
+ :database => db.path[1..-1],
+ :encoding => 'utf8'
+ )
+end
+
 enable :sessions
 helpers MainHelper, ModelHelper
