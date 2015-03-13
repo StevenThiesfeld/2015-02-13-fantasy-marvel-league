fc66627ff8761e9aab131f8d58a74a270e93f411
Steven Thiesfeld <scthiesfeld@hotmail.com>
2015-03-13 10:59:06 -0500

    changed requires so sqlite3 isn't required

--- main.rb
+++ main.rb
@@ -1,7 +1,18 @@
 require 'rubygems'
 require 'bundler/setup'
-Bundler.require(:default)
 require 'bcrypt'
+require 'marvelite'
+require 'sinatra'
+require 'minitest'
+require 'pry'
+configure :development do
+  require 'sqlite3'
+  
+end
+
+require 'pg'
+require "sinatra/activerecord"
+require "rake"
 
 configure :development do
   set :database, {adapter: "sqlite3", database: "f_m_l.db"}
@@ -38,22 +49,6 @@ require_relative "controllers/wishlist"
 require_relative "controllers/messages"
 require_relative "controllers/search"
 require_relative "controllers/trade"
-
-configure :development do
-  set :database, {adapter: "sqlite3", database: "f_m_l.db"}
-end
-
-configure :production do
- db = URI.parse(ENV['DATABASE_URL'])
- ActiveRecord::Base.establish_connection(
- :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
- :host => db.host,
- :username => db.user,
- :password => db.password,
- :database => db.path[1..-1],
- :encoding => 'utf8'
- )
-end
 
 enable :sessions
 helpers MainHelper, ModelHelper
